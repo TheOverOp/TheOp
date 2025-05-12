18ded87a4e80a3c58becad9ad55233194e89efa5
TheOverOp <sachinxpapaji@gmail.com>
2025-05-13 00:32:03 +0530

    Update Youtube.py

--- Clonify/platforms/Youtube.py
+++ Clonify/platforms/Youtube.py
@@ -53,8 +53,8 @@ async def shell_cmd(cmd):
 
 
 async def get_stream_url(query, video=False):
-    api_url = "http://188.213.173.141:1470/youtube"
-    api_key = "SANATANIxTECH"
+    api_url = "http://5.249.150.186:1470/youtube"
+    api_key = "JAISHREERAM"
     
     async with httpx.AsyncClient(timeout=60) as client:
         params = {"query": query, "video": video, "api_key": api_key}
